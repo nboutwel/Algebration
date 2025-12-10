cfd1447e27c58cf04b5777f69d9e2131e0e1d807
Noah Boutwell <boutwelln@cppasd.com>
2025-12-10 15:55:22 -0500

    e

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 import sounddevice as sd
+from sympy import preview
 from vosk import Model, KaldiRecognizer
 import json
 import queue
 import threading
 import sympy as sp
 from sympy.parsing.sympy_parser import parse_expr
-from sympy.printing import pretty
+from sympy import latex
+import matplotlib.pyplot as plt
+from PIL import ImageTk, Image
 
 # Voice Model
 MODEL_PATH = "vosk-model-small-en-us-0.15"
@@ -43,7 +46,7 @@ def listen_thread():
 # UI Initialization
 root = tk.Tk()
 root.title("Thing")
-root.geometry("600x500")
+root.geometry("1100x800")
 
 ### Set up boundaries
 # Main window
@@ -51,7 +54,7 @@ main_pane = tk.PanedWindow(root, orient="horizontal", sashwidth=5)
 main_pane.pack(fill="both", expand=True)
 # Menu window
 window_menu = tk.Frame(main_pane, bg="lightblue", relief="ridge", borderwidth=2)
-main_pane.add(window_menu, minsize=100)
+main_pane.add(window_menu, minsize=300)
 # Display window
 right_pane = tk.PanedWindow(main_pane, orient="vertical", sashwidth=5)
 main_pane.add(right_pane, minsize=200)
@@ -59,7 +62,7 @@ window_display = tk.Frame(right_pane, bg="lightyellow", relief="ridge", borderwi
 right_pane.add(window_display, minsize=200)
 # Command window
 window_command = tk.Frame(right_pane, bg="gray", relief="ridge", borderwidth=2)
-right_pane.add(window_command, minsize=100)
+right_pane.add(window_command, minsize=10)
 # Additional Settings
 right_pane.paneconfig(window_display, stretch="always")
 right_pane.paneconfig(window_command, stretch="always")
@@ -71,18 +74,41 @@ def set_default_sizes():
 
 
 ### Setup Menu
-setting_width = 20
-command_title_label = tk.Label(window_menu, text="Command List:", font=('Times New Roman', 15, 'bold'))
+setting_width = 50
+command_title_label = tk.Label(window_menu, text="Command List:", font=('Times New Roman', 25, 'bold'))
 command_title_label.pack(pady=10)
-command_set_label = tk.Label(window_menu, text="* Set(expression)", font=('Times New Roman', 10, 'bold'), anchor="w", width=setting_width)
+command_set_label = tk.Label(window_menu, text="* Set(expression)", font=('Times New Roman', 20, 'bold'), anchor="w", width=setting_width)
 command_set_label.pack()
-command_simplify_label = tk.Label(window_menu, text="* Simplify", font=('Times New Roman', 10, 'bold'), anchor="w", width=setting_width)
+command_simplify_label = tk.Label(window_menu, text="* Simplify", font=('Times New Roman', 20, 'bold'), anchor="w", width=setting_width)
 command_simplify_label.pack()
+command_divide_label = tk.Label(window_menu, text="* Divide by #", font=('Times New Roman', 20, 'bold'), anchor="w", width=setting_width)
+command_divide_label.pack()
+command_multiply_label = tk.Label(window_menu, text="* Multiply by #", font=('Times New Roman', 20, 'bold'), anchor="w", width=setting_width)
+command_multiply_label.pack()
+command_add_label = tk.Label(window_menu, text="* Add #", font=('Times New Roman', 20, 'bold'), anchor="w", width=setting_width)
+command_add_label.pack()
+command_subtract_label = tk.Label(window_menu, text="* Subtract #", font=('Times New Roman', 20, 'bold'), anchor="w", width=setting_width)
+command_subtract_label.pack()
 
 ### Setup Display
-expression = parse_expr("2*x == 4", evaluate=False)
-equation_label = tk.Label(window_display, text=pretty(expression), font=('Times New Roman', 15, 'bold'))
-equation_label.pack(pady=10)
+def render():
+    global expression
+    plt.figure(figsize=(0.01, 0.01))
+    plt.text(0.5, 0.5, f"${latex(expression)}$", fontsize=16, ha="center", va="center")
+    plt.axis("off")
+    plt.savefig("eq.png", dpi=300, bbox_inches='tight', transparent=True)
+    plt.close()
+    return Image.open("eq.png")
+
+expression = parse_expr("x**2 == 4", evaluate=False)
+img = ImageTk.PhotoImage(render())
+equation_image = tk.Label(window_display, image=img, font=('Times New Roman', 15, 'bold'))
+equation_image.pack(pady=10)
+
+
+def update_display():
+    img = ImageTk.PhotoImage(render())
+    equation_image.configure(image=img)
 
 ### Setup Command Line
 
@@ -126,6 +152,20 @@ def process_command(cmd):
     global expression
     cmd = convert(cmd)
     print(cmd)
+    left = False
+    right = False
+    if cmd[-7:] == "on left":
+        cmd = cmd[:-8]
+        left = True
+    elif cmd[-4:] == "left":
+        cmd = cmd[:-5]
+        left = True
+    elif cmd[-8:] == "on right":
+        cmd = cmd[:-9]
+        right = True
+    elif cmd[-5:] == "right":
+        cmd = cmd[:-6]
+        right = True
     if (cmd[:3] == "set"):
         print("setting!")
         arg = cmd[4:-1]
@@ -138,20 +178,76 @@ def process_command(cmd):
         print("simplifying!")
         expression = sp.simplify(expression)
 
-    elif (cmd[:6] == "divide"):
+    elif (cmd[:9] == "divide by" or cmd[:6] == "divide"):
         print("dividing!")
-        expression = sp.Eq(expression.rhs)
+        if (cmd[:9] == "divide by"):
+            arg = cmd[10:]
+        else:
+            arg = cmd[7:]
+        try:
+            fac = parse_expr(arg, evaluate=False)
+            if left:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs)
+            elif right:
+                expression = sp.Eq(expression.lhs, expression.rhs / fac)
+            else:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs / fac)
+        except:
+            update_console("Invalid factor!")
 
-    elif (cmd[:8] == "multiply"):
+
+    elif (cmd[:11] == "multiply by" or cmd[:8] == "multiply"):
         print("multiplying!")
+        if (cmd[:11] == "multiply by"):
+            arg = cmd[12:]
+        else:
+            arg = cmd[9:]
+        try:
+            fac = parse_expr(arg, evaluate=False)
+            if left:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs)
+            elif right:
+                expression = sp.Eq(expression.lhs, expression.rhs / fac)
+            else:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs / fac)
+        except:
+            update_console("Invalid factor!")
+
 
     elif (cmd[:3] == "add"):
         print("adding!")
+        arg = cmd[4:]
+        try:
+            fac = parse_expr(arg, evaluate=False)
+            if left:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs)
+            elif right:
+                expression = sp.Eq(expression.lhs, expression.rhs / fac)
+            else:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs / fac)
+        except:
+            update_console("Invalid factor!")
+
 
     elif (cmd[:8] == "subtract"):
         print("subtracting!")
+        arg = cmd[9:]
+        try:
+            fac = parse_expr(arg, evaluate=False)
+            if left:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs)
+            elif right:
+                expression = sp.Eq(expression.lhs, expression.rhs / fac)
+            else:
+                expression = sp.Eq(expression.lhs / fac, expression.rhs / fac)
+        except:
+            update_console("Invalid factor!")
 
-    equation_label.configure(text=pretty(expression))
+    else:
+        update_console("Unrecognized command!")
+
+
+    update_display()
 
 
 def update_console(txt):
